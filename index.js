111542efb35b9a7c86e8da5f4cdf0fef102806d7
Nashed <nashed@mhs.usk.ac.id>
2026-05-10 20:38:37 +0700

    Update cors

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://koeta-frontend.vercel.app',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/api/auth', authRoutes)
